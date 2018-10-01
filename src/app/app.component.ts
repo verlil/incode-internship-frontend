ba01eae4c4f7f35ddfc631b6eb3c117a14ffa191
Max <shevchenkomaksym93@gmail.com>
2018-10-01 10:41:35 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'incode-internship-frontend';
+  title: string = 'incode-internship-frontend';
 }
