342dcd56a4b82561e7eb18eab5584f2efc891428
Anton_S <cortezguitar@gmail.com>
2018-10-01 17:13:13 +0300

    core module basic structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent
   ],
   imports: [
     BrowserModule
