da496db7a66c63dfa8c3323d07913b4b1efa36d7
Andrew Gumenniy <gumenniy.oik@gmail.com>
2018-10-10 11:44:49 +0300

    header

--- src/app/core/containers/header/header.component.ts
+++ src/app/core/containers/header/header.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Store, select } from '@ngrx/store';
+import { State } from '../../../@store';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+import * as coreStore from '../../@store';
+import * as rootStore from '../../../@store';
+import { UserAuthModel } from '../../../shared/models/UserAuthModel';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent  {
+  isAuthenticated$: Observable<boolean>;
+  userLogin$: Observable<string>;
+  constructor (private store: Store<State>) {
+    this.authenticatedCheck();
+    this.userLogin$ = this.store.pipe(select(coreStore.selectUser)).pipe(
+      map((user: UserAuthModel) => user.login)
+    );
+  }
+
+  onLogout(): void {
+    this.store.dispatch(new coreStore.LogOutAction());
+    this.authenticatedCheck();
+    this.store.dispatch(new rootStore.Go({path: ['login']}));
+  }
+
+  authenticatedCheck (): void {
+    this.isAuthenticated$ = this.store.pipe(select(coreStore.selectLoginState)).pipe(
+      filter((state: coreStore.LoginState) => state.loaded),
+      map((state: coreStore.LoginState) => state.isAuthenticated)
+    );
+  }
 }
